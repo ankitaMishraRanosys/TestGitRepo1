1c63c06f317653ef84c09e40c13b5c18ec8c7f9d
Ankita Mishra <ankita.mishra@ranosys.com>
2021-09-21 10:07:37 +0530

    First Commit in t3

---
+++ Test3.java
@@ -0,0 +1,13 @@
+class Test3{
+      
+     public static void main(String []args){
+       
+          int a=8;
+          int b=65;
+
+
+          int c = a*b;
+
+        System.out.println("Multiplication of a and b is-" + c);
+     }
+}

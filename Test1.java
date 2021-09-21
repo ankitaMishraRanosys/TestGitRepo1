a4205e9e5f4d225e45e5db6a9582d111ea9eee9c
Ankita Mishra <ankita.mishra@ranosys.com>
2021-09-21 09:56:08 +0530

    Second Commit in this repo

---
+++ Test1.java
@@ -0,0 +1,11 @@
+include java.util.*;
+
+class test{
+  
+  public static void main(String []args){
+
+   System.out.println("Hello World");
+   System.out.println("Ankita is a very good girl.");
+
+ }
+}
